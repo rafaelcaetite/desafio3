62c0e8eb5df52a379cde6b4f8f656747c0ba4073
Rafael Martins <102500171+rafaelcaetite@users.noreply.github.com>
2022-03-28 19:00:04 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,5 @@
+n1=float(input('Digite um número:'))
+n2=float(input('Digite outro número:'))
+s=n1+n2
+#print('O resultado da soma entre',n1,'e',n2,'é',s)
+print(f'A soma entre {n1} e {n2} vale {s}')
